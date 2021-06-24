d18cdd12b00f3e2756406d10b92079086da92cd2
Jazhiel Arturo Cossío López <jazhiel12a@gmail.com>
2021-06-24 00:32:08 -0500

    Initial commit

---
+++ EXENTACION1/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace EXENTACION1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DGV1 = new System.Windows.Forms.DataGridView();
+            this.PRODUCTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CANTIDAD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PRECIO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SUBTOTAL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DESCUENTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TOTAL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.BTN2 = new System.Windows.Forms.Button();
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.TXT2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TXT1 = new System.Windows.Forms.TextBox();
+            this.CB1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.DGV1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1413, 686);
+            this.panel1.TabIndex = 0;
+            // 
+            // DGV1
+            // 
+            this.DGV1.AllowUserToAddRows = false;
+            this.DGV1.BackgroundColor = System.Drawing.Color.White;
+            this.DGV1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PRODUCTO,
+            this.CANTIDAD,
+            this.PRECIO,
+            this.SUBTOTAL,
+            this.DESCUENTO,
+            this.TOTAL});
+            this.DGV1.Location = new System.Drawing.Point(480, 112);
+            this.DGV1.Name = "DGV1";
+            this.DGV1.Size = new System.Drawing.Size(646, 448);
+            this.DGV1.TabIndex = 1;
+            // 
+            // PRODUCTO
+            // 
+            this.PRODUCTO.HeaderText = "PRODUCTO";
+            this.PRODUCTO.Name = "PRODUCTO";
+            // 
+            // CANTIDAD
+            // 
+            this.CANTIDAD.HeaderText = "CANTIDAD";
+            this.CANTIDAD.Name = "CANTIDAD";
+            // 
+            // PRECIO
+            // 
+            this.PRECIO.HeaderText = "PRECIO";
+            this.PRECIO.Name = "PRECIO";
+            // 
+            // SUBTOTAL
+            // 
+            this.SUBTOTAL.HeaderText = "SUBTOTAL";
+            this.SUBTOTAL.Name = "SUBTOTAL";
+            // 
+            // DESCUENTO
+            // 
+            this.DESCUENTO.HeaderText = "DESCUENTO";
+            this.DESCUENTO.Name = "DESCUENTO";
+            // 
+            // TOTAL
+            // 
+            this.TOTAL.HeaderText = "TOTAL";
+            this.TOTAL.Name = "TOTAL";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1408, 57);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(605, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PUNTO DE VENTA";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel3.Controls.Add(this.BTN2);
+            this.panel3.Controls.Add(this.BTN1);
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.TXT2);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.TXT1);
+            this.panel3.Controls.Add(this.CB1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(1, 59);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(273, 628);
+            this.panel3.TabIndex = 1;
+            // 
+            // BTN2
+            // 
+            this.BTN2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BTN2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BTN2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BTN2.Location = new System.Drawing.Point(30, 519);
+            this.BTN2.Name = "BTN2";
+            this.BTN2.Size = new System.Drawing.Size(178, 68);
+            this.BTN2.TabIndex = 9;
+            this.BTN2.Text = "RESET";
+            this.BTN2.UseVisualStyleBackColor = false;
+            this.BTN2.Click += new System.EventHandler(this.BTN2_Click);
+            // 
+            // BTN1
+            // 
+            this.BTN1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BTN1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BTN1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BTN1.Location = new System.Drawing.Point(30, 402);
+            this.BTN1.Name = "BTN1";
+            this.BTN1.Size = new System.Drawing.Size(178, 68);
+            this.BTN1.TabIndex = 8;
+            this.BTN1.Text = "CALCULAR";
+            this.BTN1.UseVisualStyleBackColor = false;
+            this.BTN1.Click += new System.EventHandler(this.BTN1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(11, 312);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(257, 29);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "¿APLICA DESCUENTO?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // TXT2
+            // 
+            this.TXT2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT2.Location = new System.Drawing.Point(11, 241);
+            this.TXT2.Name = "TXT2";
+            this.TXT2.Size = new System.Drawing.Size(167, 33);
+            this.TXT2.TabIndex = 5;
+            this.TXT2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXT2_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(6, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PRECIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(11, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CANTIDAD";
+            // 
+            // TXT1
+            // 
+            this.TXT1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT1.Location = new System.Drawing.Point(11, 143);
+            this.TXT1.Name = "TXT1";
+            this.TXT1.Size = new System.Drawing.Size(167, 33);
+            this.TXT1.TabIndex = 2;
+            this.TXT1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXT1_KeyPress);
+            // 
+            // CB1
+            // 
+            this.CB1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB1.FormattingEnabled = true;
+            this.CB1.Items.AddRange(new object[] {
+            "LECHE",
+            "HUEVO",
+            "CHORIZO"});
+            this.CB1.Location = new System.Drawing.Point(11, 54);
+            this.CB1.Name = "CB1";
+            this.CB1.Size = new System.Drawing.Size(167, 30);
+            this.CB1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(11, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "PRODUCTO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(829, 594);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 32);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "EL TOTAL ES:\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(1005, 594);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 32);
+            this.label6.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(1410, 687);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView DGV1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRODUCTO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CANTIDAD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRECIO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SUBTOTAL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DESCUENTO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TOTAL;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button BTN2;
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox TXT2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TXT1;
+        private System.Windows.Forms.ComboBox CB1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EXENTACION1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXENTACION1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //      
+            CB1.SelectedIndex = 0;
+        }
+
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+            string  TXTPRODUCTO, TXTCANTIDAD, TXTPRECIO, TXTSUBTOTAL,TXTDESCUENTO,TXTTOTAL,ERROR;
+            double CANTIDAD, PRECIO, SUBTOTAL, DESCUENTO, TOTAL,SUB,DESC2,TOTAL2;
+
+            TXTPRODUCTO = CB1.Text;
+            TXTCANTIDAD=TXT1.Text;
+            TXTPRECIO=TXT2.Text;
+            TXTSUBTOTAL="0";
+            TXTDESCUENTO="0";
+            TXTTOTAL="0";
+            TOTAL = 0;
+            TOTAL2 = 0;
+            ERROR = "";
+            TXT1.BackColor = Color.White;
+            TXT2.BackColor = Color.White;
+
+
+
+
+            //VALIDACIONES
+            if (string.IsNullOrWhiteSpace(TXTCANTIDAD))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO CANTIDAD";
+                TXT1.BackColor = Color.DarkRed;
+
+            }
+            if (string.IsNullOrWhiteSpace(TXTPRECIO))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO PRECIO";
+                TXT2.BackColor = Color.DarkRed;
+
+            }
+            if (ERROR != "")
+            {
+                MessageBox.Show(ERROR, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+
+
+
+
+            SUBTOTAL = Convert.ToDouble(TXTCANTIDAD)*Convert.ToDouble(TXTPRECIO);
+            TOTAL = Convert.ToDouble(TXTCANTIDAD) * Convert.ToDouble(TXTPRECIO);
+
+
+            if (checkBox1.Checked == true)
+            {
+                TXTDESCUENTO = "20";
+                DESCUENTO=(SUBTOTAL*0.20);
+                TOTAL =SUBTOTAL-DESCUENTO ;
+            }
+            TXTSUBTOTAL = SUBTOTAL.ToString();
+            TXTTOTAL = TOTAL.ToString();
+
+
+   
+
+
+
+            DGV1.Rows.Add(TXTPRODUCTO, TXTCANTIDAD, TXTPRECIO, TXTSUBTOTAL, TXTDESCUENTO, TXTTOTAL);
+            foreach (DataGridViewRow row1 in DGV1.Rows)
+            {
+                string CHECAR = "";
+                CHECAR = Convert.ToString(DGV1[4, row1.Index].Value);
+                if (CHECAR == "20")
+                {
+                    DGV1[4, row1.Index].Style.BackColor = Color.Red;
+                    DGV1[5, row1.Index].Style.BackColor = Color.Red;
+                }
+
+                TOTAL2 += Convert.ToDouble(DGV1[5, row1.Index].Value);
+            }
+
+            label6.Text = Convert.ToString(TOTAL2);
+        }
+
+        private void TXT1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space ||  e.KeyChar == '-');
+        }
+
+        private void TXT2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Space || e.KeyChar == '-');
+        }
+
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            TXT1.Text = "";
+            TXT2.Text = "";
+            label6.Text = "";
+            CB1.SelectedIndex = 1;
+            this.DGV1.Rows.Clear();
+     
+            TXT1.BackColor = Color.White;
+            TXT2.BackColor = Color.White;
+        }
+    }
+}
